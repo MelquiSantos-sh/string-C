20385fa47752fac27cf8a4124ccb118a9f1e927f
Melquizedequedos <melquizedequesantos13@gmail.com>
2026-05-20 08:12:57 -0300

    1 comit

---
+++ exemplo.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char nome[50];
+    char sobrenome[50];
+    int tam
+        printf("Digite seu nome: ");
+    fgets(nome, sizeof(nome), stidin);
+    nome[strlen(nome) - 1] = ´\0´; // Remove o caractere de nova linha
+    printf("digite seu sobrenome: ");
+    fgets(sobrenome, sizeof(sobrenome), stdin);
+    sobrenome[strlen(sobrenome) - 1] = '\0'; // Remove
+    prinf("Ola,%s\n", nome);
+    tam = strlen(nome);
+    prinf("seu nome tem %d caracteres\n", tam);
+    strcat(nome, sobrenome);
+    prinf("seu nome tem %d caracteres\n", tam);
+
+    return 0;
+}
---
+++ exstr1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char lua[15], cheia[15];
+    int resultado[30];
+
+    
+
+    return 0;
+}
