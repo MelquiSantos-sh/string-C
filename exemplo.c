ea3a2815b6e7724ca4aee81ca6971115703d5c0a
Melquizedequedos <melquizedequesantos13@gmail.com>
2026-05-20 21:40:04 -0300

    ajustes

--- exemplo.c
+++ exemplo.c
@@ -5,18 +5,21 @@ int main()
 {
     char nome[50];
     char sobrenome[50];
-    int tam
+    int tam;
+
+  
         printf("Digite seu nome: ");
-    fgets(nome, sizeof(nome), stidin);
-    nome[strlen(nome) - 1] = ´\0´; // Remove o caractere de nova linha
-    printf("digite seu sobrenome: ");
-    fgets(sobrenome, sizeof(sobrenome), stdin);
-    sobrenome[strlen(sobrenome) - 1] = '\0'; // Remove
-    prinf("Ola,%s\n", nome);
-    tam = strlen(nome);
-    prinf("seu nome tem %d caracteres\n", tam);
-    strcat(nome, sobrenome);
-    prinf("seu nome tem %d caracteres\n", tam);
+            fgets(nome, sizeof(nome), stdin);
+            nome[strlen(nome) - 1] = '\0'; // Remove o caractere de nova linha
+        printf("digite seu sobrenome: ");
+            fgets(sobrenome, sizeof(sobrenome), stdin);
+            sobrenome[strlen(sobrenome) - 1] = '\0'; // Remove o caractere de nova linha
+        printf("Ola,%s\n", nome);
+            tam = strlen(nome);
+        printf("seu nome tem %d caracteres\n", tam);
+            strcat(nome, sobrenome);
+            tam = strlen(nome);
+        printf("seu nome tem %d caracteres\n", tam);
 
     return 0;
 }
--- exstr1.c
+++ exstr1.c
@@ -2,10 +2,23 @@
 #include <string.h>
 int main()
 {
-    char lua[15], cheia[15];
-    int resultado[30];
+    char op1[15], op2[15];
+    int completo;
 
-    
+        printf("Digite uma palavra: ");
+            fgets(op1, sizeof(op1), stdin);
+            op1[strlen(op1) - 1] = '\0'; // Remove o caractere de nova linha
+        printf("digite outra palavra: ");
+            fgets(op2, sizeof(op2), stdin);
+            op2[strlen(op2) - 1] = '\0'; // Remove o caractere de nova linha
+        printf("Ola,%s\n", op1);
+            completo = strlen(op1);
+        printf("sua palavra tem %d caracteres\n", completo);
+            strcat(op1, op2);
+            completo = strlen(op1);
+        printf("sua palavra tem %d caracteres\n", completo);
+        printf("Sua palavra completa e %s\n", op1);
+        printf("sua palavra tem %d caracteres\n", completo);
 
     return 0;
 }
